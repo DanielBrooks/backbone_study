059e79529fea44265793fec9bd42e74a5b9e2f03
daniel <daniel@example.com>
2015-07-31 17:45:54 +0300

    adding haml to html compilation and prettifing it (which is not very perfect)

--- gulp_testing/gulpfile.js
+++ gulp_testing/gulpfile.js
@@ -3,7 +3,9 @@
 (function(r) {
   var gulp = r('gulp'),
       sass = r('gulp-sass'),
-      cssbeautify = r('gulp-cssbeautify');
+      cssbeautify = r('gulp-cssbeautify'),
+      haml = r('gulp-haml'),
+      prettify = r('gulp-prettify');
 
   gulp.task('sass-compressed', function() {
     gulp.src(
@@ -24,10 +26,22 @@
     .pipe(gulp.dest('build/css'));
   });
 
-  gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['sass']);
+  gulp.task('haml', function() {
+    gulp.src(
+      'haml/*.haml'
+    )
+    .pipe(haml())
+    .pipe(prettify({
+      'indent_size': 2
+    }))
+    .pipe(gulp.dest('build/html'));
   });
 
-  gulp.task('default', ['sass', 'watch']);
+  gulp.task('watch', function() {
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('haml/*.haml', ['haml']);
+  });
+
+  gulp.task('default', ['haml', 'sass', 'watch']);
 
 })(require);
