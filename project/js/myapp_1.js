70ebdab1a8f3828e484f65b64ce6011ec5d9c486
daniel <daniel@example.com>
2015-09-16 18:42:44 +0300

    moving model validation logic into model's method (as per Backbone manual)

--- project/js/myapp_1.js
+++ project/js/myapp_1.js
@@ -1,3 +1,24 @@
+Helpers = {
+  showError: function($block, isInsert, text) {
+    var template = _.template($('#error-template').html())({text: text}).trim();
+    if (isInsert) {
+      $(template).insertAfter($block);
+    }
+    else {
+      $block.append(template);
+    }
+  },
+  removeError: function($block) {
+    if ($block.is('[data-error-text]')) {
+      $block.remove();
+    }
+    else {
+      $block.find('[data-error-text]').remove();
+    }
+  }
+}
+
+
 $(function() {
 
   var Note = Backbone.Model.extend({
@@ -6,6 +27,26 @@ $(function() {
       return {
         title: 'New title',
         content: 'no content yet'
+      }
+    },
+    validate: function(attrs, options) {
+      var errors = false;
+      if (attrs.title.length < 3) {
+        attrs.title = 'Title can\'t be shorter than 3 symbols.';
+        errors = true;
+      }
+      else {
+        delete attrs.title;
+      }
+      if (!attrs.content.length) {
+        attrs.content = 'Note can\'t be blank.';
+        errors = true;
+      }
+      else {
+        delete attrs.content;
+      }
+      if (errors) {
+        return attrs;
       }
     }
 
@@ -53,58 +94,37 @@ $(function() {
       this.$title = this.$noteForm.find('[data-note-title]');
       this.$content = this.$noteForm.find('[data-note-content]');
       this.listenTo(Notes, 'add', this.addOne);
+      this.listenTo(Notes, 'invalid', this.noteError);
 
       Notes.fetch();
+    },
+    noteError: function(error) {
+      var er = error.validationError,
+          prop;
+      for (prop in er) {
+        if (prop == 'title') {
+          Helpers.showError(this.$title, true, er[prop]);
+        }
+        else if (prop == 'content') {
+          Helpers.showError(this.$content, true, er[prop]);
+        }
+      }
     },
     render: function() {
       console.log(2);
     },
     addOne: function(passedModel) {
       console.log(3);
+      if (!passedModel.isValid()) return;
       var view = new NoteView({model: passedModel});
       this.notesList.append(view.render().el);
     },
-    createNote: function(bb) {
-      var validation = this.newNoteValidation();
-      if (validation) {
-        Notes.create(validation);
-      }
-    },
-    newNoteValidation: function() {
-      var note = {
-            title: undefined,
-            content: undefined
-          },
-          $title = this.$title,
-          $titleBlock = $title.closest('.form-group'),
-          $content = this.$content,
-          $contentBlock = $content.closest('.form-group');
-
-      if ($title.val().length > 3) {
-        note.title = $title.val();
-        $titleBlock.removeClass('has-error');
-      }
-      else {
-        $titleBlock.addClass('has-error');
-      }
-
-      if ($content.val().length) {
-        note.content = $content.val();
-        $contentBlock.removeClass('has-error');
-      }
-      else {
-        $contentBlock.addClass('has-error');
-      }
-      return this.validateData(note);
-    },
-    validateData: function(obj) {
-      var prop;
-      for (prop in obj) {
-        if (typeof(obj[prop]) == 'undefined') {
-          return false;
-        }
-      }
-      return obj;
+    createNote: function(e) {
+      Helpers.removeError(this.$noteForm);
+      Notes.create({
+        title: this.$title.val(),
+        content: this.$content.val()
+      });
     }
 
   });
